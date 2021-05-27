209cb69aea8ecb8387d2c913617af7981ee2109e
Krishna Chaitanya <Krishna.Chaitanya@version1.com>
2021-05-27 20:59:00 +0100

    Added Components and shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {SharedModule} from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OverviewComponent } from './pages/overview/overview.component';
+import { CountryWiseComponent } from './pages/country-wise/country-wise.component';
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    OverviewComponent,
+    CountryWiseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
